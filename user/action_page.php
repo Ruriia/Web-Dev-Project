d64e5293672a6857ec57a05b5d16119de54f6211
Darmadi <darmadic199@gmail.com>
2019-04-17 21:16:58 +0700

    registration

--- user/action_page.php
+++ user/action_page.php
@@ -4,7 +4,13 @@
     $key = connection();
     
 
-    $sql = "INSERT INTO ticket(email,nim,subject,question,category,priority) VALUES (?,?,?,?,?,?)";
+    $sql = "INSERT INTO ticket(email,nim,subject_ticket,question,category,priority) VALUES (?,?,?,?,?,?)";
+    $data = [
+        $_POST['nama'],
+        $_POST['student_id'],
+        $_POST['subject']
+
+    ]
 
 
 ?>
