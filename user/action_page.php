d4d023c58cdb3a145e3a389bb31b0190401e47fd
alfon <alfonsusanto@gmail.com>
2019-04-17 21:21:23 +0700

    validasi

--- user/action_page.php
+++ user/action_page.php
@@ -3,14 +3,26 @@
 
     $key = connection();
     
+    $check = "SELECT nim FROM msdata where nim = ?";
+    $check1 = [$_POST['student_id']];
+
+    $result = $key->prepare($check);
+
+    $datanim = $result->fetch($check1);
+
+    if($datanim == $check1){
+    
 
     $sql = "INSERT INTO ticket(email,nim,subject_ticket,question,category,priority) VALUES (?,?,?,?,?,?)";
     $data = [
         $_POST['nama'],
         $_POST['student_id'],
         $_POST['subject']
+    ];
 
-    ]
+    }else{
+        echo "NIM anda tidak terdaftar";
+    }
 
 
 ?>
