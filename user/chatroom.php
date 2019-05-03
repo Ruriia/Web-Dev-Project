48c26d275f3833d650055a9a0404cbf00109d88f
Fata <fatahakasviyanto@gmail.com>
2019-05-03 20:47:30 +0700

    edit dikit

--- user/chatroom.php
+++ user/chatroom.php
@@ -118,7 +118,7 @@ li.dropdown {
             </a>
             <div class="dropdown-menu" style="letter-spacing:0px;">
                 <a class="dropdown-item" href="accountsettings.php">Account Settings</a>
-                <a data-target="#modalLogout" data-toggle="modal" class="dropdown-item" id="navlogout" href="#modalLogout">Logout</a>
+                <button type="button" id="tombol" class="dropdown-item" onclick="sweetclick()">Logout</button>
             </div>
         </li>
         </ul>
@@ -188,9 +188,26 @@ li.dropdown {
   <p>&copy; Flexbox Gang.</p>
 </footer>
 
+<script>
+  function sweetclick(){
+    Swal.fire({
+    title: 'Are you sure you want to logout?',
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes!'
+  }).then((result) => {
+    if (result.value) {
+      window.location.href = "../logout.php";
+    }
+  })
+  }
+</script>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="dist/sweetalert2.all.min.js"></script>
 </body>
 </html>
