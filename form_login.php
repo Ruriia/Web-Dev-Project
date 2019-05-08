ed1a31798c929d40cedb8b3bd810d6bd27a6d9c6
Fata <fatahakasviyanto@gmail.com>
2019-05-08 22:03:09 +0700

    login form

--- form_login.php
+++ form_login.php
@@ -34,11 +34,32 @@
         font-size: 15px;
         font-weight: bold;
     }
+
+    .login-block{
+float:left;
+width:100%;
+padding : 50px 0;
+}
+.banner-sec{background:url(https://static.pexels.com/photos/33972/pexels-photo.jpg)  no-repeat left bottom; background-size:cover; min-height:500px; border-radius: 0 10px 10px 0; padding:0;}
+.container{background:#fff; border-radius: 10px; box-shadow:15px 20px 0px rgba(0,0,0,0.1);}
+.carousel-inner{border-radius:0 10px 10px 0;}
+.carousel-caption{text-align:left; left:5%;}
+.login-sec{padding: 50px 20px; position:relative;}
+.login-sec .copy-text{position:absolute; width:80%; bottom:20px; font-size:13px; text-align:center;}
+.login-sec .copy-text i{color:#6A95CC;}
+.login-sec .copy-text a{color:#6A95CC;}
+.login-sec h2{margin-bottom:30px; font-weight:800; font-size:30px; color: #6A95CC;}
+.login-sec h2:after{content:" "; width:140px; height:5px; background:#6A95CC; display:block; margin-top:20px; border-radius:3px; margin-left:auto;margin-right:auto}
+.btn-login{ color:#fff; font-weight:600;}
+.banner-text{width:70%; position:absolute; bottom:40px; padding-left:20px;}
+.banner-text h2{color:#fff; font-weight:600;}
+.banner-text h2:after{content:" "; width:100px; height:5px; background:#FFF; display:block; margin-top:20px; border-radius:3px;}
+.banner-text p{color:#fff;}
 </style>
 
 </head>
-<body style="background-color:#3366ff;">
-<div class="login-form">
+<body style="background-color:#6A95CC;">
+<!--<div class="login-form">
     <form action="login_validation.php" method="post">
         <h3 class="text-center">KRS Helpdesk Login</h3>
         <br/>
@@ -62,7 +83,52 @@
             <a href="#" class="pull-right">Forgot Password?</a>
         </div>        
     </form>
+</div>-->
+
+<section class="login-block">
+    <div class="container">
+	<div class="row">
+		<div class="col-md-4 login-sec">
+		    <h2 class="text-center">KRS Helpdesk</h2>
+		    <form class="login-form" action="login_validation.php" method="post">
+        <div class="form-group">
+            <?php if($_SESSION['loginfail'] == 3): ?>
+              <input type="text" class="form-control" name="email" placeholder="E-mail Student" required="required" value="<?= $_SESSION['email']; ?>">
+            <?php else: ?>
+              <input type="text" class="form-control" name="email" placeholder="E-mail Student" required="required">
+            <?php endif;?>
+        </div>
+        <div class="form-group">
+            <input type="password" id="pass" class="form-control" name="katasandi" placeholder="Password" required="required">
+            <input type="checkbox" onclick="visiblePassword()" class="mt-3"> Show Password
+        </div>
+  
+  
+    <div class="form-group">
+    <button type="submit" class="btn btn-login btn-block btn-success">Login</button>
+    <a href="index.php" id="home" name="home" class="btn btn-secondary btn-block">Cancel</a>
+    </div>
+  
+</form>
+		</div>
+		<div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>Got Any Problems?</h2>
+            <p>We can help you, just simply log in to proceed.</p>
+        </div>	
+  </div>
+    </div>
+            </div>	   
+		    
+		</div>
+	</div>
 </div>
+</section>
 
 <?php if ($_SESSION['loginfail'] == 2): ?>
 <script>
